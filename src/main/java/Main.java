151e7f14ff86a2e1a73ede526bf5906b309ebafd
Igor Figueiredo <igfigueiredo@outlook.com>
2024-02-06 11:24:10 +0100

    TestCode class: Dummt data test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,6 @@
-import nl.igf.tekst.WordFrequencyData;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Counting words App");
-
-        String tekst = """
-                    Ordina understands the power of disruptive technologies /and cooperation in a digital ecosystems. 
-                    And we can #put ourselves in ** our customers’ shoes and in their digital and business and world. 
-                """;
-
-        WordFrequencyData count = new WordFrequencyData();
-        int hf = count.calculateHighestFrequency(tekst);
-        int wf = count.calculateFrequencyForWord(tekst, "digital");
-
-        System.out.println("Highest frequency: " + hf);
-        System.out.println("Frequency for a word: " + wf);
-//        System.out.println("in: " + in);
+        TestCode.testWithDummyData();
     }
 }
